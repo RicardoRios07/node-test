277bbb897e0c093e7e9e32795300d997eb791f49
RickDeuS <riosr3060@gmail.com>
2023-06-01 21:41:12 -0500

    backend

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Rutas estáticas para los archivos generados por React
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// Ruta para la página principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
