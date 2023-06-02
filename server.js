f38a80d96ccc6ee917449bb7bd6b9435735dd37d
RickDeuS <riosr3060@gmail.com>
2023-06-01 22:51:20 -0500

    produc

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 // Rutas estáticas para los archivos generados por React
 app.use(express.static(path.join(__dirname, 'client', 'build')));
